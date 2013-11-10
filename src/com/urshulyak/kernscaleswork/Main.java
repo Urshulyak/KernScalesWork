e180e31aa4ea8253b86b47282cb4e73e0e56c0da
Urshulyak Yaroslav <y.urshulyak@gmail.com>
2013-11-10 13:35:18 +0200

    Base  application

---
+++ src/com/urshulyak/kernscaleswork/Main.java
@@ -0,0 +1,28 @@
+package com.urshulyak.kernscaleswork;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ one line to give the program's name and an idea of what it does.
+ Copyright (C) 2013 Urshulyak Yaroslav
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ */
+public class Main  extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/com/urshulyak/kernscaleswork/config/Config.java
@@ -0,0 +1,18 @@
+package com.urshulyak.kernscaleswork.config;
+
+/**
+ * one line to give the program's name and an idea of what it does.
+ * Copyright (C) 2013 Urshulyak Yaroslav
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ */
+public class Config {
+
+}
